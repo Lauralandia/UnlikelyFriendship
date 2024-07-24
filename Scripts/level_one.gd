c5a2e9ee04f66f42335bad1d78ce31d03effe62d
delauraen <22270079+delauraen@users.noreply.github.com>
2024-07-23 21:12:12 -0700

    forest level 1
    first pass of the first level, tutorial to be integrated later

--- Scripts/level_one.gd
+++ Scripts/level_one.gd
@@ -3,8 +3,8 @@ extends Node2D
 @onready var assassin_player = $mainPlayer/Assassin
 
 func _ready():
-	$mainPlayer/Assassin/Camera2D.limit_top = -1650
-	$mainPlayer/Assassin/Camera2D.limit_right = 3455
+	$mainPlayer/Assassin/Camera2D.limit_top = -3650
+	$mainPlayer/Assassin/Camera2D.limit_right = 10455
 	$mainPlayer/Assassin/Camera2D.limit_left = -500
 	assassin_player.global_position.y = -850
 	assassin_player.global_position.x = -200
